f26ce20db2ee27e7f300e6e74835e4b7df3bdd0a
nigar-basarova <nigarbasarova@gmail.com>
2024-04-22 20:15:10 +0400

    pecode

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import'bootstrap/dist/js/bootstrap'
+import './App.css'
+
+
+//* komponentler gelecek
+import Navbar from './components/navbar'
+
+
+
+
+function App() {
+  
+
+  return (
+   <Navbar/>
+  )
+}
+
+export default App

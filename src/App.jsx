f2e0a2c7f931b8351a88a09afff85c5cce195dcc
nigar-basarova <nigarbasarova@gmail.com>
2024-05-08 23:59:36 +0400

    обноваа

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import'bootstrap/dist/js/bootstrap'
 import './App.css'
 
-
 //* komponentler gelecek
-import Navbar from './components/Navbar'
+import Navbar from './Navbar'
 import Introduction from './components/Introduction'
 
 
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,34 +1,40 @@
-import React from 'react';
-import App from '../App';
 
+import React from 'react';
+import App from './App';
+import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
+<>
+<div className="mobile-menu"></div>
+
     <nav className="navbar navbar-expand-lg">
       <div className="container">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" to="#">
           <img src="public/assets/images/logo.svg" alt="" />
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="#">What we do</a>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+         
+          <li className="nav-item">
+              <a className="nav-link" href="#">What we do</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Career</a>
+              <a className="nav-link" href="/#">Career</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Contact</a>
+              <a className="nav-link" href="/#">Contact</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Blog</a>
+              <a className="nav-link" href="/#">Blog</a>
             </li>
           </ul>
         </div>
       </div>
     </nav>
+    </>
   );
 }
 

97d5dd40d09e9cb91eb57579d2ff61c525d7345e
nigar-basarova <nigarbasarova@gmail.com>
2024-05-12 03:48:59 +0400

    new

--- src/components/ArticlesSection.jsx
+++ src/components/ArticlesSection.jsx
@@ -1,18 +1,42 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
 
 const ArticlesSection = () => {
   return (
     <div>
-        <div className="articles-two">
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+      <div className="articles-two ">
+        <div className="row">
+          <div className="col-12 col-md-6">
+            {/* properties-xususiyyetler props */}
+           
+            <Card kartinShekli="/assets/images/blog1.jpeg" kartinBashligi="yeniXeber"/>
+          </div>
+          <div className="col-12 col-md-6 ">
+            <Card />
+          </div>
         </div>
-    </div>
-  )
-}
+      </div>
 
-export default ArticlesSection
+      <div className="articles-tree  ">
+<div className="row">
+<div className="col-12 col-md-6 col-lg-4">
+          <Card />
+        </div>
+        <div className="col-12 col-md-6 col-lg-4">
+          <Card />
+        </div>
+        <div className="col-12 col-md-6 col-lg-4">
+          <Card />
+        </div>
+       
+</div>
+      </div>
+    </div>
+
+
+
+
+  );
+};
+
+export default ArticlesSection;

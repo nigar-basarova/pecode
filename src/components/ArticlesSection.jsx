4e9743c3dc0d5a09602e1e1c932701f2fcdeb56c
nigar-basarova <nigarbasarova@gmail.com>
2024-05-13 02:59:55 +0400

    new

--- src/components/ArticlesSection.jsx
+++ src/components/ArticlesSection.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Card from "./Card";
+import axios from "axios";
 
 const ArticlesSection = () => {
+
+    // massivin parcalanmasi(array destructuring)
+const[melumatlar,setMelumat]=useState([])
+useEffect(()=>{
+axios.get("https://newsapi.org/v2/everything?q=tesla&from=2024-04-12&sortBy=publishedAt&apiKey=77028469fb3d491bb847bff9e9448a8a")
+.then ((xeberler)=> {
+    setMelumat(xeberler.data.articles)
+})
+}, [])
+
+
   return (
     <div>
       <div className="articles-two ">
@@ -9,26 +21,24 @@ const ArticlesSection = () => {
           <div className="col-12 col-md-6">
             {/* properties-xususiyyetler props */}
            
-            <Card kartinShekli="/assets/images/blog1.jpeg" kartinBashligi="yeniXeber"/>
+            <Card kartinShekli="/assets/images/blog1.jpeg" kartinBashligi="yeniXeber"kartinParaqrafi="Burada sizin reklaminiz ola biler:)"/>
           </div>
           <div className="col-12 col-md-6 ">
-            <Card />
+          <Card kartinShekli="/assets/images/blog2.png" kartinBashligi="React 2018"kartinParaqrafi="Meqalemiz"/>
           </div>
         </div>
       </div>
 
       <div className="articles-tree  ">
 <div className="row">
-<div className="col-12 col-md-6 col-lg-4">
-          <Card />
-        </div>
+{
+    melumatlar .map(birXeber=> (
         <div className="col-12 col-md-6 col-lg-4">
-          <Card />
-        </div>
-        <div className="col-12 col-md-6 col-lg-4">
-          <Card />
-        </div>
-       
+        <Card  kartinShekli={birXeber.urlToImage} kartinBashligi={birXeber.title.slice(0,50)} kartinParaqrafi={birXeber.description}/>
+      </div>
+
+    ))
+}
 </div>
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
+import './Card.css'
+// ES6 2015
+// obyektin parcalanmasi object distructuring
 
-const Card = () => {
+
+
+
+const Card = ({kartinBashligi, kartinShekli,kartinParaqrafi}) => {
+   
   return (
-    <div className='card'>
-<img src="../assets/images/blog1.jpeg" alt="" />
-<div className="card-body">
-    <h1>The truth about ReactJS</h1>
-    <p>Sigh, it’s like we can’t catch a break. So much has happened over the past happened happened over the past happened.....</p>
+    <div className='card border-0 px-3'>
+<img src={kartinShekli} alt=""className='blog-img' />
+<div className="card-body p-0 py-3">
+    <h1 className='card-title'>{kartinBashligi}</h1>
+    <p>{kartinParaqrafi}...</p>
+    <div>
+
+    <img src="./assets/icon/favicon.svg" alt="" />
+    <span className='moderator'>Pecode team <span id="22.03.2024"></span></span>
 </div>
+
+</div>
+
+
     </div>
   )
 }

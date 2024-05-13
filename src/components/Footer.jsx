ba1bae6afba6765d1d410d2bf96683c214237cb3
nigar-basarova <nigarbasarova@gmail.com>
2024-05-13 04:44:43 +0400

    new

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,6 +10,19 @@ const Footer = () => {
 <div className="col-12 col-md-4">
     <img src="/assets/images/footerLogo.svg" alt="" />
 </div>
+<div className="col-12 col-md-4">
+  <h4 className='text-white'>Contact Us</h4>
+  <div className="mail">
+    <p className='text-secondary'>
+    If you want to implement your business ideas: </p>
+ <a href="mailto:" className='text-white'> hello@pecodesoftware.com</a>
+  </div>
+  <div className="mail">
+    <p className='text-secondary'>
+    If you are looking for new career opportunities: </p>
+ <a href="mailto:" className='text-white'> career@pecodesoftware.com</a>
+  </div>
+</div>
       </div>
    </div>
 

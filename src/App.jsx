3a923cccd1fc769e39c1bd0940648e5dc27f47ec
nigar-basarova <nigarbasarova@gmail.com>
2024-05-13 04:22:55 +0400

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import'bootstrap/dist/js/bootstrap'
 import './App.css'
+import Footer from './components/Footer'
 import ArticlesSection from './components/ArticlesSection'
 //* komponentler gelecek
 import Navbar from './Navbar'
@@ -14,10 +15,15 @@ function App() {
 
   return (
     <>
+   <div className='container'>
     <Navbar/>
     <ArticlesSection/>
     <Introduction/>
-    </>
+   
+    </div>
+    
+     <Footer/>
+     </>
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Footer.css'
+
+
+const Footer = () => {
+  return (
+    <div className=' bg-dark py-5'>
+   <div className="container footer">
+   <div className='row'>
+<div className="col-12 col-md-4">
+    <img src="/assets/images/footerLogo.svg" alt="" />
+</div>
+      </div>
+   </div>
+
+
+    </div>
+  )
+}
+
+export default Footer
